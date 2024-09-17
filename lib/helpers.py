13cc47a6f450ef7177682028feb8df52da89c275
Pauline-WN <ngurupauline0@gmail.com>
2024-09-17 21:01:46 +0300

    added helper functions

---
+++ lib/helpers.py
@@ -0,0 +1,10 @@
+def display_menu():
+    print("Welcome to PetCare Hub!")
+    print("1. Manage Pets")
+    print("2. Manage Owners")
+    print("3. Manage Care Schedule")
+    print("4. Manage Vet Visits")
+    print("0. Exit")
+
+def handle_invalid_input():
+    print("Invalid choice. Please try again.")
